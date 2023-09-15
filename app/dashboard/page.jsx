887c42b1d5e45820ff1f21c92ad10e52814a4335
med134 <81263260+med134@users.noreply.github.com>
2023-09-15 18:29:57 +0100

    dashborad

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -61,75 +61,77 @@ const Dashboard = () => {
   }
   if (session.status === "authenticated") {
     return (
-      <div className="p-16 flex justify-between lg:inline-block lg:justify-center lg:items-center sm:p-8">
+      <>
         <h1 className="text-xl font-lexend text-gray-700 px-5">
           Add New template or component
         </h1>
-        <form className="p-4 text-left text-gray-700" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Title"
-            className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring sm:px-2"
-          />
-          <input
-            type="text"
-            placeholder="Desc"
-            className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
-          />
-          <input
-            type="text"
-            placeholder="Image"
-            className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
-          />
-          <input
-            type="text"
-            placeholder="Link"
-            className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
-          />
-          <input
-            type="text"
-            placeholder="category"
-            className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
-          />
-          <textarea
-            placeholder="put your code here"
-            className="h-44 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
-          />
-          <button className="rounded-md font-semibold py-2 w-full bg-violet-600 text-light ml-4 hover:bg-purple-400">
-            Post Now
-          </button>
-        </form>
-        <div className="w-full p-6 rounded-lg">
-          {isLoading ? (
-            <Loading />
-          ) : (
-            data?.map((post) => (
-              <div key={post._id}>
-                <div className="">
-                  <img
-                    className="rounded-md object-cover"
-                    src={post.image}
-                    alt="image_post"
-                    width={350}
-                    height={80}
-                  />
+        <div className="p-16 flex justify-between lg:inline-block lg:justify-center lg:items-center sm:p-8">
+          <form className="p-4 text-left text-gray-700" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Title"
+              className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring sm:px-2"
+            />
+            <input
+              type="text"
+              placeholder="Desc"
+              className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
+            />
+            <input
+              type="text"
+              placeholder="Image"
+              className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
+            />
+            <input
+              type="text"
+              placeholder="Link"
+              className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
+            />
+            <input
+              type="text"
+              placeholder="category"
+              className="h-12 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
+            />
+            <textarea
+              placeholder="put your code here"
+              className="h-44 w-full max-w-full rounded-md border m-4 bg-white px-5 text-sm outline-none focus:ring"
+            />
+            <button className="rounded-md font-semibold py-2 w-full bg-violet-600 text-light ml-4 hover:bg-purple-400">
+              Post Now
+            </button>
+          </form>
+          <div className="w-full p-6 rounded-lg">
+            {isLoading ? (
+              <Loading />
+            ) : (
+              data?.map((post) => (
+                <div key={post._id}>
+                  <div className="">
+                    <img
+                      className="rounded-md object-cover"
+                      src={post.image}
+                      alt="image_post"
+                      width={350}
+                      height={80}
+                    />
+                  </div>
+                  <div className="inline-flex justify-between">
+                    <h2 className="text-xl p-3 px-2 sm:text-sm font-lexend py-3">
+                      {post.title}
+                    </h2>
+                    <span
+                      onClick={() => handleDelete(post._id)}
+                      className="p-1 bg-red-400 text-light m-3 font-semibold rounded-md cursor-pointer hover:bg-red-500 bottom-1"
+                    >
+                      delete
+                    </span>
+                  </div>
                 </div>
-                <div className="inline-flex justify-between">
-                  <h2 className="text-xl p-3 px-2 sm:text-sm font-lexend py-3">
-                    {post.title}
-                  </h2>
-                  <span
-                    onClick={() => handleDelete(post._id)}
-                    className="p-1 bg-red-400 text-light m-3 font-semibold rounded-md cursor-pointer hover:bg-red-500 bottom-1"
-                  >
-                    delete
-                  </span>
-                </div>
-              </div>
-            ))
-          )}
+              ))
+            )}
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 };
