7cec108ff107390e44bcf83a1e1d6db85d5bcfd4
med134 <81263260+med134@users.noreply.github.com>
2023-09-16 16:37:36 +0100

    dashboard modification

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,42 @@
-/* eslint-disable @next/next/no-img-element */
-
 import React from "react";
 import AddNewPost from "../components/AddNewPost";
 
 export const metadata = {
-  title: "dashboard",
+  title: "medCode | Dashboard",
+  description: `Elevate Your Web Development with Free Tailwind CSS Templates & Components & code source Our meticulously designed frontend dashboard templates and components`,
+  keywords: [
+    "Web Development",
+    "tailwind css free components",
+    "tailwind css free components",
+    "frontend templates",
+    "how to learn programming",
+    "free components sketchup ",
+    "python programming",
+    "django frontend templates",
+    "Artificial Intelligence",
+    "best programming languages",
+    "rust programming language",
+    " Programming Languages",
+    "Software Engineering",
+    "tailwind css",
+    "free templates",
+    "free components",
+    "bootstrap",
+    "free code",
+    "Front-end",
+    "UI/UX Design",
+    "Frameworks",
+    "Best Practices",
+    "Web Design",
+    "Learning Resources",
+    "Code Snippets",
+  ],
+  alternates: {
+    canonical: `/dashboard`,
+    languages: {
+      en: `/en/dashboard`,
+    },
+  },
 };
 const Dashboard = () => {
   return <AddNewPost />;
