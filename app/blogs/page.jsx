aed689493aa69fcf03b5b9f59dc1d0fc2042db3f
med134 <81263260+med134@users.noreply.github.com>
2023-11-05 18:10:30 +0100

    add

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -39,7 +39,7 @@ export const metadata = {
       tutorials.`,
     images: [
       {
-        url: "/app/images/postera.png",
+        url: "https://i.ibb.co/vztYGDH/Untitled.png",
         width: "400",
         height: "300",
       },
