6023307275dc080e33dcaa9fe19efbfdea250468
med134 <81263260+med134@users.noreply.github.com>
2023-12-05 19:22:32 +0100

    theme

--- app/context/ThemeProvider.js
+++ app/context/ThemeProvider.js
@@ -3,10 +3,10 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("dark");
 
   const toggle = () => {
-    setMode((prev) => (prev === "light" ? "dark" : "light"));
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
   return (
